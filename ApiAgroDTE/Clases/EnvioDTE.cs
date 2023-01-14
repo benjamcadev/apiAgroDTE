e35bfab621708c4b33c6a9e92a0ce094860a8a20
Benjamin Cortes <benjamca@hotmail.com>
2023-01-14 15:50:23 -0300

    agregue en la funcion verificarDte que guarde el token que consulta en un archivo txt

--- ApiAgroDTE/Clases/EnvioDTE.cs
+++ ApiAgroDTE/Clases/EnvioDTE.cs
@@ -334,8 +334,10 @@ namespace ApiAgroDTE.Clases
 
         public string enviarSobre(string archivo, string rutEmisor, string rutEmpresa)
         {
-            WebRequest request = WebRequest.Create("http://localhost:81/WebServiceEnvioDTE/EnvioSobreDTE.asmx/enviarSobreSII?archivo=" + archivo + "&rutEmisor=" + rutEmisor + "&rutEmpresa=" + rutEmpresa);
+            
+            //WebRequest request = WebRequest.Create("http://localhost:81/WebServiceEnvioDTE/EnvioSobreDTE.asmx/enviarSobreSII?archivo=" + archivo + "&rutEmisor=" + rutEmisor + "&rutEmpresa=" + rutEmpresa);
             //WebRequest request = WebRequest.Create("http://192.168.1.9:90/WebServiceEnvioDTE_Maullin/EnvioSobreDTE.asmx/enviarSobreSII?archivo="+ archivo + "&rutEmisor="+ rutEmisor + "&rutEmpresa="+ rutEmpresa);
+            WebRequest request = WebRequest.Create("http://192.168.1.9:90/WebServiceEnvioDTE/EnvioSobreDTE.asmx/enviarSobreSII?archivo="+ archivo + "&rutEmisor="+ rutEmisor + "&rutEmpresa="+ rutEmpresa);
             request.Method = "GET";
             WebResponse response = request.GetResponse();
             string respuestaEnvio = "";
@@ -371,8 +373,8 @@ namespace ApiAgroDTE.Clases
 
                 //METODO RESTSHARP----------------------------------------------------
                 //SETEAMOS PARAMETROS DEL CLIENTE
-                //var client = new RestClient("https://rahue.sii.cl/recursos/v1/boleta.electronica.envio");// PRODUCCION
-                var client = new RestClient("https://pangal.sii.cl/recursos/v1/boleta.electronica.envio");// CERTIFICACION
+                var client = new RestClient("https://rahue.sii.cl/recursos/v1/boleta.electronica.envio");// PRODUCCION
+                //var client = new RestClient("https://pangal.sii.cl/recursos/v1/boleta.electronica.envio");// CERTIFICACION
                 client.UserAgent = "Mozilla/4.0 ( compatible; PROG 1.0; Windows NT)";
 
                 //SETEAMOS PARAMETROS DE LA REQUEST
@@ -382,8 +384,8 @@ namespace ApiAgroDTE.Clases
                 request.AddHeader("Accept", "application/json");
                 request.AlwaysMultipartFormData = true;
                 request.AddHeader("Content-Type", "multipart/form-data");
-                //request.AddHeader("Host", "rahue.sii.cl"); //PRODUCCION
-                request.AddHeader("Host", "pangal.sii.cl"); //CERTIFICACION
+                request.AddHeader("Host", "rahue.sii.cl"); //PRODUCCION
+                //request.AddHeader("Host", "pangal.sii.cl"); //CERTIFICACION
                 
                 //AGREGAMOS LOS PARAMETROS
                 request.AddParameter("rutSender", pRutEmisor);
@@ -472,8 +474,8 @@ namespace ApiAgroDTE.Clases
             {
                 //NO EXISTE TOKEN ACTIVO, SE PIDE UNO
                 //OBTENER SEMILLA
-               
-                var client = new RestClient("https://apicert.sii.cl/recursos/v1/");
+               var client = new RestClient("https://api.sii.cl/recursos/v1/");
+                //var client = new RestClient("https://apicert.sii.cl/recursos/v1/");
                 var request = new RestRequest("boleta.electronica.semilla", Method.GET);
                 var response = client.Execute(request);
 
